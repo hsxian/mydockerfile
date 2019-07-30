08b5ab583dc30b95544002c01b17eec87a7ac37b
hsxian <940100027@qq.com>
2019-07-30 12:47:27 +0800

    调整启动hbase根目录

--- bigdata/cluster/bash/master-start.sh
+++ bigdata/cluster/bash/master-start.sh
@@ -3,27 +3,26 @@ mkdir -p /usr/local/zookeeper/data
 echo "1" > /usr/local/zookeeper/data/myid
 
 #每个容器下都要启动 zookpeer
-cd /usr/local/zookeeper/bin
-./zkServer.sh  start
+cd /usr/local/zookeeper
+./bin/zkServer.sh  start
 echo -e "\033[32msleep 1m\033[0m"
 sleep 1m
 echo "........"
 
 #启动Hdfs
-cd /usr/local/hadoop/sbin
-./start-dfs.sh
-
-#启动hbase
-cd /usr/local/hbase/bin
-./start-hbase.sh
-nohup ./hbase thrift -p 9090 start &
-
+cd /usr/local/hadoop
+./sbin/start-dfs.sh
 #启动yarn
-cd /usr/local/hadoop/sbin
-./start-yarn.sh
+./sbin/start-yarn.sh
 
 #启动spark
-cd /usr/local/spark/sbin
-./start-all.sh
+cd /usr/local/spark
+./sbin/start-all.sh
+
+
+#启动hbase
+cd /usr/local/hbase
+./bin/start-hbase.sh
+nohup ./bin/hbase thrift -p 9090 start &
 
 cd /usr/local/
--- bigdata/cluster/bash/slave02-start.sh
+++ bigdata/cluster/bash/slave02-start.sh
@@ -4,18 +4,16 @@ mkdir -p /usr/local/zookeeper/data
 echo "3" > /usr/local/zookeeper/data/myid
 
 #每个容器下都要启动 zookpeer
-cd /usr/local/zookeeper/bin
-./zkServer.sh  start
+cd /usr/local/zookeeper
+./bin/zkServer.sh  start
 echo -e "\033[32msleep 1m\033[0m"
 sleep 1m
 echo "........"
 
 #启动Hdfs
-cd /usr/local/hadoop/sbin
-./start-dfs.sh
-
+cd /usr/local/hadoop
+./sbin/start-dfs.sh
 #启动yarn
-cd /usr/local/hadoop/sbin
-./start-yarn.sh
+./sbin/start-yarn.sh
 
 cd /usr/local/
